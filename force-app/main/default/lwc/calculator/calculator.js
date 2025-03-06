34e6af8181b10e2349be698e5261b2d8d2062d6a
Prince Saini <princesaini0144@gmail.com>
2025-03-07 00:04:25 +0530

    Complete the mathematic ligic

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,30 +1,31 @@
 import { LightningElement } from 'lwc';
 
 export default class Calculator extends LightningElement {
-    numberOne;
-    numberTwo;
+    numberOne = "";
+    numberTwo = "";
+    result = 0;
 
     addHandler(event){
-
+        this.result = parseInt(this.numberOne) + parseInt(this.numberTwo);
     }
 
     subHandler(event){
-
+        this.result = parseInt(this.numberOne) - parseInt(this.numberTwo);
     }
 
     mulHandler(event){
-
+        this.result = parseInt(this.numberOne) * parseInt(this.numberTwo);
     }
 
     divHandler(event){
-
+        this.result = parseInt(this.numberOne) / parseInt(this.numberTwo);
     }
 
     changeHandlerNumberOne(event){
-
+        this.numberOne = event.target.value;
     }
 
     changeHandlerNumberTwo(event){
-
+        this.numberTwo = event.target.value;
     }
 }
