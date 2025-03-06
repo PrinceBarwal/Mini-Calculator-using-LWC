a4cf0487e383084d730321b32f5c9e4f07bcc7ea
Prince Saini <princesaini0144@gmail.com>
2025-03-06 23:56:41 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+
+export default class Calculator extends LightningElement {
+    numberOne;
+    numberTwo;
+
+    addHandler(event){
+
+    }
+
+    subHandler(event){
+
+    }
+
+    mulHandler(event){
+
+    }
+
+    divHandler(event){
+
+    }
+
+    changeHandlerNumberOne(event){
+
+    }
+
+    changeHandlerNumberTwo(event){
+
+    }
+}
