8b0ab4e88207d597332ecaf3b4a1c6e6a4ab6e5c
Prince Saini <princesaini0144@gmail.com>
2025-03-07 00:25:03 +0530

    modified the code to make it optimize

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -5,27 +5,56 @@ export default class Calculator extends LightningElement {
     numberTwo = "";
     result = 0;
 
-    addHandler(event){
-        this.result = parseInt(this.numberOne) + parseInt(this.numberTwo);
+    // addHandler(event){
+    //     this.result = parseInt(this.numberOne) + parseInt(this.numberTwo);
+    // }
+
+    // subHandler(event){
+    //     this.result = parseInt(this.numberOne) - parseInt(this.numberTwo);
+    // }
+
+    // mulHandler(event){
+    //     this.result = parseInt(this.numberOne) * parseInt(this.numberTwo);
+    // }
+
+    // divHandler(event){
+    //     this.result = parseInt(this.numberOne) / parseInt(this.numberTwo);
+    // }
+
+    calculateInput(event){
+        let labelElement = event.target.label;
+        if(labelElement === 'ADD'){
+            this.result = parseInt(this.numberOne) + parseInt(this.numberTwo);
+        }
+        else if(labelElement === 'SUB'){
+            this.result = parseInt(this.numberOne) - parseInt(this.numberTwo);
+        }
+        else if(labelElement === 'MUL'){
+            this.result = parseInt(this.numberOne) * parseInt(this.numberTwo);
+        }
+        else if(labelElement === 'DIV'){
+            this.result = parseInt(this.numberOne) / parseInt(this.numberTwo);
+        }
+        
     }
 
-    subHandler(event){
-        this.result = parseInt(this.numberOne) - parseInt(this.numberTwo);
+    // changeHandlerNumberOne(event){
+    //     this.numberOne = event.target.value;
+    // }
+
+    // changeHandlerNumberTwo(event){
+    //     this.numberTwo = event.target.value;
+    // }
+
+    changeHandler(event){
+        let name = event.target.name;
+        let value = event.target.value;
+        if(name === 'number1'){
+            this.numberOne = value;
+        }
+        else if(name === 'number2'){
+            this.numberTwo = value;
+        }
     }
 
-    mulHandler(event){
-        this.result = parseInt(this.numberOne) * parseInt(this.numberTwo);
-    }
-
-    divHandler(event){
-        this.result = parseInt(this.numberOne) / parseInt(this.numberTwo);
-    }
-
-    changeHandlerNumberOne(event){
-        this.numberOne = event.target.value;
-    }
-
-    changeHandlerNumberTwo(event){
-        this.numberTwo = event.target.value;
-    }
 }
